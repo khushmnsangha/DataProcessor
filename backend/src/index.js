2d6954b99c230535b9fba6daf1018678f43121a6
khushmnsangha <anmolsangha02@gmail.com>
2025-02-21 18:48:30 +0530

    made code deploy ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 
@@ -9,8 +10,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
 const bfhlRoutes = require("./routes/bfhlRoutes");
 app.use("/bfhl", bfhlRoutes);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });  
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
