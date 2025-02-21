3005e1dd70771661a6ffbbe0f960161a7b3c59f8
khushmnsangha <anmolsangha02@gmail.com>
2025-02-21 18:59:13 +0530

    made code deploy ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,13 +10,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../../frontend/build")));
 
 const bfhlRoutes = require("./routes/bfhlRoutes");
 app.use("/bfhl", bfhlRoutes);
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/build/index.html"));
   });  
 
 const PORT = process.env.PORT || 5000;
